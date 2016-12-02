0e45c405e42127b590c08e3b12a8b05e97a8b64c
Kendsdavs <kendra.wieck@gmail.com>
2016-12-02 10:24:10 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const React = require('react')
+const minesweeper = require('minesweeper')
+
+const App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Christmas Minesweeper</h1>
+      </div>
+    )
+  }
+})
+
+module.exports = App
